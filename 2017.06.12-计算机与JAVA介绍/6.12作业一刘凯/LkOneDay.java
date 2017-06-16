1aa7629a8bb1071d0a18f56f9d6ab1022e9166a8
AmbitionWolf <1404741290@qq.com>
2017-06-16 08:45:13 +0800

    Big_data

---
+++ 2017.06.12-计算机与JAVA介绍/6.12作业一刘凯/LkOneDay.java
@@ -0,0 +1,10 @@
+//编写一个Java程序
+class LkOneDay{
+	//编写一个程序执行入口-Main函数
+	public static void main(String[] args){
+		//向控制台输出一条语句
+		System.out.println("  *  ");
+		System.out.println(" *** ");
+		System.out.println("*****");
+	}
+}
---
+++ 2017.06.12-计算机与JAVA介绍/代码/HelloWorld.java
@@ -0,0 +1,19 @@
+// Decompiled by Jad v1.5.8e2. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://kpdus.tripod.com/jad.html
+// Decompiler options: packimports(3) fieldsfirst ansi space 
+// Source File Name:   HelloWorld.java
+
+import java.io.PrintStream;
+
+class HelloWorld
+{
+
+	HelloWorld()
+	{
+	}
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+	}
+}
